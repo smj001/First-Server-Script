b81b9c2d4863cff07d0d07b7a368cef43fbb5dd7
Mohammad Javadi <s.m.javadi001@gmail.com>
2021-11-04 17:04:30 +0330

    change apt proxy

--- set-proxy-apt.sh
+++ set-proxy-apt.sh
@@ -2,7 +2,7 @@
 touch /etc/apt/apt.conf.d/proxy.conf
 cat << EOF > /etc/apt/apt.conf.d/proxy.conf
 Acquire {
-  HTTP::proxy "http://arazcloud:4r4zCl0uD@65.21.28.172:1433/";
-  HTTPS::proxy "http://arazcloud:4r4zCl0uD@65.21.28.172:1433/";
+  HTTP::proxy "http://65.21.13.253:990/";
+  HTTPS::proxy "http://65.21.13.253:990/";
 }
 EOF
