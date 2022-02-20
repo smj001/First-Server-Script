ec55e3e4020a748dbed75a6779d118967b7f274e
smj001 <s.m.javadi001@gmail.com>
2022-02-20 16:18:55 +0330

    bug fix openvpn

--- openvpn-docker.sh
+++ openvpn-docker.sh
@@ -7,7 +7,7 @@ docker run -v smj-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn 
 docker run -v smj-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
 docker run -v smj-service:/etc/openvpn -d -p 6969:1194 --cap-add=NET_ADMIN kylemanna/openvpn
 docker run -v smj-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full $user nopass
-docker run -v smjservice:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient $user > $user.ovpn
+docker run -v smj-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient $user > $user.ovpn
 echo " ======================= openvpn deployed successfully ======================= "
 echo " "
 echo " ===== you can use this product with [ $user.ovpn ] file in this directory  ===== "
