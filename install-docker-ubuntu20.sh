a786325ff43e19e07fb84975fdb8e7403b716797
s.m.javadi001 <s.m.javadi001@gmail.com>
2022-02-26 15:09:48 +0000

    change docker verison in docker ubuntu 20 for kubespray

--- install-docker-ubuntu20.sh
+++ install-docker-ubuntu20.sh
@@ -7,13 +7,13 @@ echo 'nameserver 185.51.200.2' > /etc/resolv.conf
 echo 'nameserver 178.22.122.100' >> /etc/resolv.conf
 HTTP_PROXY='https_proxy=65.21.13.253:990'
 export $HTTP_PROXY
-wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.9-1_amd64.deb
-wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
-wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.6-1_amd64.deb
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.7~3-0~ubuntu-focal_amd64.deb
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.7~3-0~ubuntu-focal_amd64.deb
 
-dpkg -i containerd.io_1.4.9-1_amd64.deb
-dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
-dpkg -i docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb
+dpkg -i containerd.io_1.4.6-1_amd64.deb
+dpkg -i docker-ce-cli_20.10.7~3-0~ubuntu-focal_amd64.deb
+dpkg -i docker-ce_20.10.7~3-0~ubuntu-focal_amd64.deb
 
 docker run hello-world
 docker ps -a
